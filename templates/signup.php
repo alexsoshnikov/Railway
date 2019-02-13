9e9a9ef34bfcdda446cc17052a32f74c11080292
alexsoshnikov <39810210+alexsoshnikov@users.noreply.github.com>
2019-02-13 13:56:48 +0300

    Страница регистрации

---
+++ templates/signup.php
@@ -0,0 +1,14 @@
+ <div class="signUp">
+                <input class="text" type="text" placeholder="Name">
+                <br>
+                <input class="text" type="text" placeholder="Surname">
+                <br>
+                <input class="text" type="text" placeholder="1111-111111">
+                <br>
+                <input class="text" type="text" placeholder="+7(900)111-11-11">
+                <br>
+                <input class="text" type="text" placeholder="Login">
+                <br>
+                <input class="text" type="text" placeholder="Password"> </div>
+            <div class="searchButton">
+                <input class="submit" type="submit" value="Sign Up"> </div>
