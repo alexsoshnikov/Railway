b854a9b47ff8f4caa062fe5db04c673abc58fbf5
alexsoshnikov <39810210+alexsoshnikov@users.noreply.github.com>
2019-02-15 19:50:07 +0300

    добавил картинку в вкладку

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require "db.php";
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="styles/style.css">
-        
+         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
